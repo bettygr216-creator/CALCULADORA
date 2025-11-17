b6b5940608797f37aae4ec7a1c3e424e21084e11
bettygr216-creator <bettygr216@gmail.com>
2025-11-16 19:00:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const display = document.getElementById("display");
+
+function append(num) {
+    display.value += num;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
+
